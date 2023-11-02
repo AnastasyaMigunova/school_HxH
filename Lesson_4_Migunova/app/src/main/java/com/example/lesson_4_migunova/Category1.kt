a463d0718a7ee9e3e670757c325e6098436f2f3f
AnastasyaMigunova <115943303+AnastasyaMigunova@users.noreply.github.com>
2023-11-02 08:35:32 +0400

    Commit

--- Lesson_4_Migunova/app/src/main/java/com/example/lesson_4_migunova/Category1.kt
+++
@@ -1,7 +0,0 @@
-package com.example.lesson_4_migunova
-
-data class Category1(
-    val image : Int,
-    val title: String,
-    val description : String,
-)
--- Lesson_4_Migunova/app/src/main/java/com/example/lesson_4_migunova/ListItem.kt
+++
@@ -1,7 +0,0 @@
-package com.example.lesson_4_migunova
-
-sealed class ListItem {
-    data class CategoryItem(val category: Category) : ListItem()
-
-    data class Category1Item(val category1: Category1) : ListItem()
-}
