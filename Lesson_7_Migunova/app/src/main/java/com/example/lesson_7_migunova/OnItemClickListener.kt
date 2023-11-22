b4066d834a882586d537b1fa80378bc4a693b6c9
AnastasyaMigunova <115943303+AnastasyaMigunova@users.noreply.github.com>
2023-11-23 02:46:32 +0400

    Commit_lesson6

--- Lesson_7_Migunova/app/src/main/java/com/example/lesson_7_migunova/OnItemClickListener.kt
+++
@@ -1,7 +0,0 @@
-package com.example.lesson_7_migunova
-
-import com.example.lesson_7_migunova.data.model.Bridge
-
-interface OnItemClickListener {
-    fun onItemClick(bridge: Bridge)
-}
--- Lesson_7_Migunova/app/src/main/java/com/example/lesson_7_migunova/data/MainApiService.kt
+++
@@ -1,9 +0,0 @@
-package com.example.lesson_7_migunova.data
-
-import com.example.lesson_7_migunova.data.model.Bridge
-import retrofit2.http.GET
-
-interface MainApiService {
-        @GET("bridges")
-        suspend fun getBridges(): List<Bridge>
-}
